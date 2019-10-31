1ae48f071c9482fc2dfb46db2f5fcb9c65e673d1
Matthew Cheng <matthewkit@gmail.com>
2019-10-31 11:21:57 +0000

    created test for overlap times Answers UCL-RITS/rsd-classwork-2019#18

---
+++ 05-testing/test_times.py
@@ -0,0 +1,18 @@
+from times import overlap_time, time_range
+
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(overlap_time(large, short))
+    result = overlap_time(large, short) 
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_class_time():
+    large = time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
+    short = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)
+    print(overlap_time(large, short))
+    result = overlap_time(large, short) 
+    expected = [('2019-10-31 10:05:00', '2019-10-31 10:55:00'), ('2019-10-31 11:05:00', '2019-10-31 11:55:00'),
+                ('2019-10-31 12:05:00', '2019-10-31 12:55:00')]
+    assert result == expected
